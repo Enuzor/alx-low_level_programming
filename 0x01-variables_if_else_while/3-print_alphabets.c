2fd68a0c6faffe56c5b71fb66a869c2b47b110eb
root <enuzor@gmail.com>
2022-09-09 08:00:38 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* print alphabets in lower and upper c */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+
+	char c;
+
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar(c);
+		putchar('\n');
+	
+	return (0);
+
+}

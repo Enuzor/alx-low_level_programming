60a3309cf8053090f6660096535018cf7ea5e80f
root <enuzor@gmail.com>
2022-09-09 14:49:50 -0700

    print 10 base

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -13,10 +13,11 @@
 int main(void)
 {
 	int num;
-	for (num =0; num <10; num++)
-		printf("%d",num);
+
+	for (num =0; num < 10; num++)
+		printf("%d", num);
 
 	printf("\n");
 
-	return(0);
+	return (0);
 }

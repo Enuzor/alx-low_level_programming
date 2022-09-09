a5d02dccdf9bb6f6e1e9f9ad5b55d9aa567d5e1f
root <enuzor@gmail.com>
2022-09-09 08:48:48 -0700

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,12 +8,14 @@
 int main(void)
 {
 	    char letter;
-
+	    
 	    for (letter = 'a'; letter <= 'z'; letter++)
-	    {
-            if (letter != 'e' && lette != 'q')
-	    putchar(letter);
+	    {    
+		    if (letter != 'e' && letter != 'q')    
+			    putchar(letter);
 	    }
+	    
 	    putchar('\n');
-
-    return(0);
+	    
+	    return (0);
+}

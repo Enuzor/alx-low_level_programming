5a000c34c8ae8ad1b8170298ac1047ea6e3d7459
root <enuzor@gmail.com>
2022-09-09 08:17:09 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,20 +7,16 @@
 
 
 /* betty style doc for function main goes there */
-
 int main(void)
-
 {
-
 	char c;
 
+	for (c = 'a'; c<= 'z'; c++)
+		putchar(c);
+	for (c = 'A'; c<= 'Z'; c++)
+		putchar(c);
+	        putchar('\n');
 
-		for (c = 'a'; c <= 'z'; c++)    
-			putchar(c);    
-		for (c = 'A'; c <= 'Z'; c++)    
-			putchar(c);        
-		        putchar('\n');              
-		
-		return (0);
+	return (0);
 
 }

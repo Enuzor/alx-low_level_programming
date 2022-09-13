924a1ebc0d9511fd38d478d5a41ab1f55daa5960
root <enuzor@gmail.com>
2022-09-13 07:39:16 -0700

    mod

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,8 +31,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar('0' + (product / 10);
-				_putchar('0' + (product % 10);
+				_putchar('0' + (product / 10));
+				_putchar('0' + (product % 10));
 			}
 		}
 		_putchar('\n');

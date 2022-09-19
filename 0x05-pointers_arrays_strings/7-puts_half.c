ae0e69623514dc15611d03e2d23880cc7400310b
root <enuzor@gmail.com>
2022-09-19 08:41:08 -0700

    mod

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - prints half of a string and a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}

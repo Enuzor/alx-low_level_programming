25b22dc2b4a7467d67cf46be6405ac50e872c4f3
root <enuzor@gmail.com>
2022-10-24 16:21:18 -0700

    tasks

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * print_listint_safe - function that prints a listint_t list
+ * @head:
+ * Return: return listint
+ * **/
+size_t print_listint_safe(const listint_t *head)
+{
+	head++;
+	return (0);
+}
